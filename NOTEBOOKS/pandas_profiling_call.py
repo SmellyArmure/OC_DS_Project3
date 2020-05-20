0a0eb28dd8d05e865d17318b928c11fe13c1c0db
Maryse <muller.maryse@gmail.com>
2020-05-20 18:27:36 +0200

    P3_Cleaning terminé (élimination des lignes sans energy ou sans nutriscore) - généré les fichiers pipfile et le requirements.txt - P3_Analysis commencé - ménage dans les fichiers.

--- NOTEBOOKS/pandas_profiling_call.py
+++
@@ -1,8 +0,0 @@
-import pandas_profiling
-# Generating a Data Profiling html report via pandas_profiling
-profile = pandas_profiling.ProfileReport(dfcopy) #, check_correlation = False
-profile.to_file("OFFacts_report2_3.html")
-profile
-# extract rejected columns (based on correlation coeff greater than 0.9)
-rejected_variables_90 = profile.get_rejected_variables(threshold=0.9)
-len(rejected_variables_90)
