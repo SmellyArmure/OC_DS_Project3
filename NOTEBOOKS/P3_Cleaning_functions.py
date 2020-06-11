021b22c50e268873594c0738edf30ca8abcfe12f
Maryse <muller.maryse@gmail.com>
2020-06-11 17:59:23 +0200

    Présentation : application + plan | P3_Cleaning : qq modifs mise en page | P3_Analysis : Imputation par knnImputer avec fonctions (exporté dans un fichier .py mais pas encore effacé du notebook)

---
+++ NOTEBOOKS/P3_Cleaning_functions.py
@@ -0,0 +1,168 @@
+'''Ensemble de fonction pour l'imputation à partir d'un modèle knn (régression ou classification)
+ pour utiliser :
+ # désigner les variables indépendantes (X) , qui ne doivent pas contenir de nan
+ var_model = ['carbohydrates_100g','categories_main']
+ # et la variable cible (y)
+ var_target = 'main_category'
+ # puis donner la dataframe complète (avec nan dans la colonne target)
+ # version cible catégorie
+ ind_to_impute, y_pr_ = Knn_impute(df, var_model = var_model, var_target=var_target,
+                                        enc_strat_cat='label', plot=False)
+ # version cible quantitative
+ ind_to_impute, y_pr_ = Knn_reg_impute(df, var_model=var_model, var_target=var_target,
+               enc_strat_cat='label', clip=(0,100), plot=True)
+ # et pour faire l'imputation :
+ df.loc[ind_to_impute, var_target] = y_pr_
+'''
+
+
+# Data Preprocessing for quantitative and categorical data with encoding options
+def data_preprocessing(df, var_model, var_target, enc_strat_cat='label'):
+    ## Data Processing
+    df_train = df[var_model+[var_target]].copy('deep')
+    if df[var_model].isna().sum().sum()!=0 :
+        print("ERROR: var_model columns should not contain nan !!!")
+        return None, None
+    else:
+        cat_cols = df_train[var_model].select_dtypes('object').columns
+        num_cols = df_train[var_model].select_dtypes(include=np.number).columns
+        # # Encoding categorical values
+        if enc_strat_cat == 'label':
+        # --- OPTION 1: Label Encoding categorical values
+            for c in cat_cols:
+                df_train[c] = LabelEncoder().fit_transform(df_train[c].values)
+        elif enc_strat_cat == 'hashing':
+        # --- OPTION 2: Feature hashing of categorical values
+            for c in cat_cols:
+                df_train[c] = df_train[c].astype('str')
+                n_feat = 5
+                hasher = FeatureHasher(n_features=n_feat, input_type='string')
+                f = hasher.transform(df_train[c])
+                arr = pd.DataFrame(f.toarray(), index=df_train.index)
+                df_train[[c+'_'+str(i+1) for i in range(n_feat)]] = pd.DataFrame(arr)
+                del df_train[c]
+                cols = list(df_train.columns)
+                cols.remove(var_target)
+                df_train = df_train.reindex(columns=cols+[var_target])
+        else:
+            print("ERROR: Wrong value of enc_strat_cat")
+            return None, None
+        # # Standardizing quantitative values
+        if len(list(num_cols)):
+            df_train[num_cols] = \
+                      StandardScaler().fit_transform(df_train[num_cols].values)
+        # Splitting in X and y, then in training and testing set
+        X = df_train.iloc[:,:-1].values
+        y = df_train.iloc[:,-1].values
+        return X, y
+
+        def naive_model_compare_r2(X_tr, y_tr, X_te, y_te, y_pr):
+    # Model
+    print('--- model: {:.3}'.format(metrics.r2_score(y_te, y_pr)))
+    # normal random distribution
+    y_pr_rand = np.random.normal(0,1, y_pr.shape)
+    print('--- normal random distribution: {:.3}'\
+          .format(metrics.r2_score(y_te, y_pr_rand)))
+    # dummy regressors
+    for s in ['mean', 'median']:
+        dum = DummyRegressor(strategy=s).fit(X_tr, y_tr)
+        y_pr_dum = dum.predict(X_te)
+        print('--- dummy regressor ('+ s +'): {:.3}'\
+              .format(metrics.r2_score(y_te, y_pr_dum)))
+
+def naive_model_compare_acc_f1(X_tr, y_tr, X_te, y_te, y_pr, average='micro'):
+    print('ooooooo CLASSIFICATION METRICS oooooooo')
+    def f1_acc(yte, ypr):
+        prec = metrics.precision_score(yte, ypr, average=average)
+        rec = metrics.recall_score(yte, ypr, average=average)
+        f1 = metrics.f1_score(yte, ypr, average=average)
+        return [f1, prec, rec]
+    # Model
+    print('--- model: f1={:.3}, precision={:.3}, recall={:.3}'\
+                                             .format(*f1_acc(y_te, y_pr)))
+    # dummy classifier
+    for s in ['stratified','most_frequent','uniform']:
+        dum = DummyClassifier(strategy=s).fit(X_tr, y_tr)
+        y_pr_dum = dum.predict(X_te)
+        print('--- dummy class. ('+ s\
+              +'): f1={:.3}, precision={:.3}, recall={:.3}'\
+                                             .format(*f1_acc(y_te, y_pr_dum)))
+
+    def plot_hist_pred_val(y_te, y_pr, y_pr_, bins=150, xlim=(0,20), short_lab=False):
+    # Plotting dispersion of data to be imputed
+    bins = plt.hist(y_te, alpha=0.5, color='b', bins=bins, density=True,
+                    histtype='step', lw=3, label='y_te (real val. from test set)')[1]
+    ax=plt.gca()
+    ax.hist(y_pr, alpha=0.5, color='g', bins=bins, density=True,
+            histtype='step', lw=3, label='y_pr (pred. val. from test set)');
+    ax.hist(y_pr_, alpha=0.5, color='r', bins=bins, density=True,
+            histtype='step', lw=3, label='y_pr_ (pred. val. to be imputed)');
+    ax.set(xlim=xlim)
+    plt.xticks(rotation=45, ha='right')
+    plt.draw()
+    if short_lab:
+        labels = [item.get_text() for item in ax.get_xticklabels()]
+        short_labels = [s[0:7]+'.' if len(s)>7 else s for s in labels]
+        ax.axes.set_xticklabels(short_labels)
+    ax.legend(loc=1)
+    plt.title("Frequency of values", fontweight='bold', fontsize=12)
+    plt.gcf().set_size_inches(6,2)
+    plt.show()
+
+# Works for both quantitative (knnregressor)
+# and categorical (knnclassifier) target features
+
+def Knn_impute(df, var_model, var_target, enc_strat_cat='label',
+                   clip=None, plot=True):
+    if df[var_target].isna().sum()==0:
+        print('NOTHING TO IMPUTE')
+        return None, None
+    else :
+        if df[var_target].dtype in ['object', 'categories']:
+            # knn classifier
+            skf = StratifiedKFold(n_splits=5, shuffle=True)
+            gsCV = GridSearchCV(KNeighborsClassifier(),
+                            {'n_neighbors': [5,7,9,11,13]},
+                            cv=skf, return_train_score=True,
+                            scoring='f1_weighted')
+            mod = 'class'
+        else:
+            # knn regressor
+            kf = KFold(n_splits=5, shuffle=True)
+            gsCV = GridSearchCV(KNeighborsRegressor(),
+                            {'n_neighbors': [3,5,7,9,11,13]},
+                            cv=kf, return_train_score=True)
+            mod = 'reg'
+        ## Data Preprocessing
+        X, y = data_preprocessing(df.dropna(subset=var_model+[var_target]),
+                                var_model=var_model, var_target=var_target,
+                                enc_strat_cat=enc_strat_cat)
+        X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.2)
+        ## Training KNN
+        gsCV.fit(X_tr, y_tr)
+        res = gsCV.cv_results_
+        ## Predicting test set with the model and clipping
+        y_pr = gsCV.predict(X_te)
+        try:
+            if clip: y_pr = y_pr.clip(*clip) # regressor option only
+        except:
+            print("ERROR: clip available for regressor option only") 
+        # Comparison with naive baselines
+        if mod == 'class':
+            naive_model_compare_acc_f1(X_tr,y_tr,X_te,y_te,y_pr,average='micro')
+        elif mod == 'class':
+            naive_model_compare_r2(X_tr,y_tr,X_te,y_te,y_pr)
+        else:
+            print("ERROR: check type of target feature...")
+        ## Predicting using knn
+        ind_to_impute = df.loc[df[var_target].isna()].index 
+        X_, y_ = data_preprocessing(df.loc[ind_to_impute], var_model=var_model,
+                                    var_target=var_target,
+                                    enc_strat_cat=enc_strat_cat)
+        # Predicting with model
+        y_pr_ = gsCV.predict(X_)
+        # Plotting histogram of predicted values
+        short_lab = True if mod == 'class' else False
+        if plot: plot_hist_pred_val(y_te, y_pr, y_pr_, short_lab=short_lab)
+        # returning indexes to impute and calculated values
+        return ind_to_impute, y_pr_
